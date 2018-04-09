506e0919fd526d3ac969f4a455bcc5b3519654b1
nevermore6264 <nevermore6264@gmail.com>
2018-04-09 10:52:28 +0700

    [Bài tập] Cài đặt thuật toán tìm kiếm nhị phân sử dụng đệ quy

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,20 +1,18 @@
 public class BinarySearch {
     static int binarySearch(int arr[], int low, int high, int value) {
         if (high >= low) {
-            int mid = (high + low) / 2;
+            int mid = low + (high - low) / 2;
             if (arr[mid] == value)
                 return mid;
-            if (arr[mid] < value) {
+            if (arr[mid] > value)
                 return binarySearch(arr, low, mid - 1, value);
-            } else if (arr[mid] > value) {
-                return binarySearch(arr, mid + 1, high, value);
-            }
+            return binarySearch(arr, mid + 1, high, value);
         }
         return -1;
     }
 
     public static void main(String[] args) {
-        int value = 100;
+        int value = 4;
         int arrNumber[] = {1, 2, 3, 33, 55, 67, 79};
         int resultIndex = binarySearch(arrNumber, 0, arrNumber.length, value);
         System.out.println("Index is: " + resultIndex);
